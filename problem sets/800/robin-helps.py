8e4f44d9878788ccde2e1f78257c7b30afcaabfc
Valluri Leela Sai Krishna <valluri4496@gmail.com>
2026-02-20 23:23:21 +0530

    Add files via upload

---
+++ problem sets/800/robin-helps.py
@@ -0,0 +1,15 @@
+t=int(input())
+for i in range(t):
+    n,k=map(int,input().split())
+    a=list(map(int,input().split()))
+    gold=0
+    people=0
+    for j in range(n):
+        if a[j]>=k:
+            gold+=a[j]
+        if a[j]==0 and gold:
+            gold-=1
+            people+=1
+        else:
+            continue          
+    print(people)
