4991b796561e96adcfac44b950fbf9a93a251ac1
Valluri Leela Sai Krishna <valluri4496@gmail.com>
2026-02-20 23:13:29 +0530

    Add files via upload

---
+++ problem sets/800/square?.py
@@ -0,0 +1,8 @@
+t=int(input())
+for i in range(t):
+    l=input()
+    lengths=l.split()
+    if lengths[0]==lengths[1]==lengths[2]==lengths[3]:
+        print("yes")
+    else:
+        print("no")
