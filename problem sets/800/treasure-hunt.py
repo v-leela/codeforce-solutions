00f67ddfe28e44b75da32fda94dcfae6222c5b55
Valluri Leela Sai Krishna <valluri4496@gmail.com>
2026-02-20 23:22:45 +0530

    Update input handling in treasure-hunt.py
    Refactor input handling and add implementation tags.

--- problem sets/800/treasure-hunt.py
+++ problem sets/800/treasure-hunt.py
@@ -1,5 +1,9 @@
-n=int(input())
-for i in range(n):
+"""
+tags: implementation, math
+"""
+
+t=int(input())
+for i in range(t):
     b,k,a=map(int,input().split())
     st=a//(b+k)
     if (st)*(b+k)+b>=a+0.5:
