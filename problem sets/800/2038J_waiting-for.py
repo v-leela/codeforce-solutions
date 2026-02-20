87940efbcc1c7136d7441f80fbfb3f88fb7116ec
Valluri Leela Sai Krishna <valluri4496@gmail.com>
2026-02-20 23:38:11 +0530

    Rename waiting-for.py to 2038J_waiting-for.py

--- problem sets/800/2038J_waiting-for.py
+++ problem sets/800/2038J_waiting-for.py
@@ -1,9 +1,12 @@
+"""
+tags: greedy, implementation
+"""
+
 n=int(input())
 P=0
 for _ in range(n):
     event=input().split()
     event[1]=int(event[1])
-    """ print(event[1]) """
     if event[0]=="P":
         P+=event[1]
     else:
