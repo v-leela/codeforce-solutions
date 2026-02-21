fdd13b51c9bf0d6e4eb65f4c315953af3f6ab25b
valluri4496-dev <your-email@gmail.com>
2026-02-21 11:30:42 +0530

    Refactor 2029A_set.py to simplify logic and remove unused multiples function

--- problem sets/800/2029A_set.py
+++ problem sets/800/2029A_set.py
@@ -2,20 +2,11 @@
 tags: greedy, math
 """
 
-def multiples(int,k):
-    multi=[]
-    for i in range(k):
-        multi.append((i+1)*int)
-    return multi
-
 t=int(input())
 for _ in range(t):
     l,r,k=map(int,input().split())
-    s=list(range(l,r+1))
-    """ print(s) """
-    count=0
-    for j in s:
-        if set(multiples(j,k))<=set(s):
-            count+=1
-    print(count)    
-    
+    if r//k>=l:
+        last_num=int(r//k)
+        print(last_num-l+1)
+    else:
+        print(0)
