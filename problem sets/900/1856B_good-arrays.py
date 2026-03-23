1ad102f0fad5ce238872f12449d8db2ad65fc41a
valluri4496-dev <your-email@gmail.com>
2026-03-23 19:16:40 +0530

    Refactor 1856B_good-arrays.py to enhance input handling and streamline logic

--- problem sets/900/1856B_good-arrays.py
+++ problem sets/900/1856B_good-arrays.py
@@ -1,5 +1,23 @@
+"""
+tags: implementation, math
+"""
+
+import numpy as np
+
 t=int(input())
 for _ in range(t):
     n=int(input())
-    a=list(map(int,input().split()))
-    print(a)
+    a=sorted(list(map(int,input().split())))
+    a=np.array(a)
+    if n==1:
+        print("no")
+    else:
+        if n%2==0:
+            b=np.array([1]*(int(n/2))+[-1]*(int(n/2)))
+        else:
+            b=np.array([2]+[1]*(int((n-3)/2))+[-1]*(int((n+1)/2)))
+        c=a+b
+        if 0 in c:
+            print("no")
+        elif 0 not in c:
+            print("yes")
