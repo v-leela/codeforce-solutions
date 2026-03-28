07505357d157ffb8f542e874bb7b95ac610ccf47
valluri4496-dev <your-email@gmail.com>
2026-03-28 22:23:33 +0530

    Refactor variable handling and add test cases in 2185D_outofmemoryerror.py

--- problem sets/1100/2185D_outofmemoryerror.py
+++ problem sets/1100/2185D_outofmemoryerror.py
@@ -1,13 +1,17 @@
+#status: not accepted (time limit exceeded)
+"""
+tags:data structures, implementation, math, two pointers
+"""
+
 t=int(input())
 for _ in range(t):
     n,m,h=map(int,input().split())
     a=list(map(int,input().split()))
-    rep=a
+    rep=a.copy()
     for k in range(m):
         b,c=map(int,input().split())
-        if rep[b-1]+c>h:
-            rep=a
-            print(rep[b-1]+c)
+        if a[b-1]+c>h:
+            a=rep
         else:
-            rep[b-1]+=c
-    print(a)
+            a[b-1]+=c
+    print(" ".join(list(map(str,a))))
