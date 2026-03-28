61c75c8dc7459507da9f1975d0fd15b6cdc6add4
valluri4496-dev <your-email@gmail.com>
2026-03-28 23:43:53 +0530

    Implement solution for "Yet Another Array Problem" with prime number calculations

---
+++ problem sets/1000/2167D_yet-another-array-problem.py
@@ -0,0 +1,33 @@
+import math
+from functools import reduce
+
+def is_prime(x):
+    if x < 2:
+        return False
+    for i in range(2, int(x**0.5) + 1):
+        if x % i == 0:
+            return False
+    return True
+
+def nth_prime(n):
+    count = 0
+    num = 1
+    while count < n:
+        num += 1
+        if is_prime(num):
+            count += 1
+    return num
+
+t=int(input())
+for _ in range(t):
+    n=int(input())
+    a=list(map(int,input().split()))
+    i=2
+    while i<10**8:
+        gcd=reduce(math.gcd,a+[nth_prime(i)])
+        if gcd==1:
+            print(nth_prime(i))
+            break
+        i+=1
+    if i==10**8:
+        print(-1)
