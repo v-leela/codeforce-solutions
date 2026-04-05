405d7987f67da16b8ebc403dd06e531e0d2e4d66
valluri4496-dev <your-email@gmail.com>
2026-04-05 16:18:12 +0530

    Refactor solution logic in E_the-67th-xor-problem.py to improve clarity and performance

--- contests/Codeforces Round 1090 (Div. 4)/E_the-67th-xor-problem.py
+++ contests/Codeforces Round 1090 (Div. 4)/E_the-67th-xor-problem.py
@@ -4,13 +4,21 @@ for _ in range(t):
     n=int(input())
     a=list(map(int,input().split()))
     res=0
-    for x in a:
+    """ for x in a:
         res^=x
     print(res)
-    """ for i in range(n-1):
+    for i in range(n-1):
         mx=min(a)
         a.remove(mx)
         for j in range(len(a)):
             a[j]=a[j]^mx
         print(a)
     print(a[0]) """
+    mx=0
+    for i in range(n):
+        b=a[:i]+a[i+1:]
+        for j in range(n-1):
+            xor=b[j]^a[i]
+            if xor>mx:
+                mx=xor
+    print(mx)
