9077f690aa6fff6a634d08d80a09ef7ab17e2803
valluri4496-dev <your-email@gmail.com>
2026-04-05 16:18:50 +0530

    Remove commented-out code and incorrect answer note in E_the-67th-xor-problem.py

--- contests/Codeforces Round 1090 (Div. 4)/E_the-67th-xor-problem.py
+++ contests/Codeforces Round 1090 (Div. 4)/E_the-67th-xor-problem.py
@@ -1,19 +1,7 @@
-#wrong answer on test 2
 t=int(input())
 for _ in range(t):
     n=int(input())
     a=list(map(int,input().split()))
-    res=0
-    """ for x in a:
-        res^=x
-    print(res)
-    for i in range(n-1):
-        mx=min(a)
-        a.remove(mx)
-        for j in range(len(a)):
-            a[j]=a[j]^mx
-        print(a)
-    print(a[0]) """
     mx=0
     for i in range(n):
         b=a[:i]+a[i+1:]
