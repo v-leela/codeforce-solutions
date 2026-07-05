e9f840a9c44093541216b7486336083a9c59e173
valluri4496-dev <your-email@gmail.com>
2026-07-05 10:28:49 +0530

    Refactor problem 2236C solution: encapsulate input handling and add random test case generation

--- problem sets/1000/2236C.py
+++ problem sets/1000/2236C.py
@@ -1,4 +1,43 @@
-a, b, x = list(map(int, input().split()))
+"""t = int(input())
+for _ in range(t):
+    a, b, x = list(map(int, input().split()))
+    count = 0
+    while a != b:
+        if b > a * x:
+            b = b // x
+        elif a > b * x:
+            a = a // x
+        elif a > b:
+            if a - b > b - a // x:
+                a = a // x
+            elif a - b > 1 and a - b > a // x - b // x:
+                a = a // x
+                b = b // x
+                count += 1
+            else:
+                b += 1
+        elif b > a:
+            if b - a > a - b // x:
+                b = b // x
+            elif b - a > 1 and b - a > b // x - a // x:
+                a = a // x
+                b = b // x
+                count += 1
+            else:
+                a += 1
+        count += 1
+    # print(a, b, "count :", count)
+
+    print(count)"""
+
+import random
+
+a = random.randint(1, 100)
+b = random.randint(1, 100)
+x = random.randint(2, 10)
+# a, b, x =
+print(a, b, x)
+
 count = 0
 while a != b:
     if b > a * x:
@@ -8,12 +47,22 @@ while a != b:
     elif a > b:
         if a - b > b - a // x:
             a = a // x
+        elif a - b > 1 and a - b > a // x - b // x:
+            a = a // x
+            b = b // x
+            count += 1
         else:
             b += 1
     elif b > a:
         if b - a > a - b // x:
             b = b // x
+        elif b - a > 1 and b - a > b // x - a // x:
+            a = a // x
+            b = b // x
+            count += 1
         else:
             a += 1
     count += 1
-print(count)
+    print(a, b, "count :", count)
+
+print(a, b, count)
